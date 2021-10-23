5819a8632d0102580d212e5e19afd927ed998200
Louis <huynguyennbk@gmail.com>
2021-10-23 20:56:30 +0700

    test: change background and text

--- src/Test.tsx
+++ src/Test.tsx
@@ -4,7 +4,7 @@ export default () => {
   const [count, setCount] = createSignal(0);
 
   return (
-    <div>
+    <div className="bg-blue-700 text-white p-4">
       <div>Count: {count()}</div>
       <button onClick={() => setCount(count() + 1)}>Add here</button>
     </div>
